f265f688dc965d8b57c86ef94ca3e6f0e3108e9a
Veeravenkatanagasai <andeveeravenkatanagasai@gmail.com>
2026-02-04 17:12:14 +0530

    Update showDetails.js

--- front-end/src/services/showDetails.js
+++ front-end/src/services/showDetails.js
@@ -1,9 +1,9 @@
 // src/services/bookingDetails.service.js
 import api from "./api"; // your global axios instance
 
-export const fetchBookings = async (page = 1, limit = 12,status="ALL") => {
+export const fetchBookings = async (page = 1, limit = 12,status="ALL",bookingId = "") => {
   const res = await api.get("/booking-details", {
-    params: { page, limit ,status},
+    params: { page, limit ,status,search: bookingId},
   });
   return res.data;
 };
